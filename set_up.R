1c05dfb87e6208710ac4f67d3dddfa4f08822ded
TomSOWI <tomklotz@gmx.net>
2023-04-13 14:43:49 +0200

    .

--- set_up.R
+++ set_up.R
@@ -4,5 +4,5 @@ use_git_config(user.name = "TomSOWI", user.email = "tomklotz@gmx.net")
 #gh_token(api_url = )
 #gitcreds::gitcreds_get()
 
-use_github(protocol = "https", auth_token = Sys.getenv("GITHUB_PAt"))
+#use_github(protocol = "https", auth_token = Sys.getenv("GITHUB_PAt"))
 #?use_github
