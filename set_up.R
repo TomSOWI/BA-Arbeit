5b0328ac9df9c654f940c590eebd7e8dfa0ca7ff
TomSOWI <tomklotz@gmx.net>
2023-04-13 14:42:21 +0200

    minor update

--- set_up.R
+++ set_up.R
@@ -1,2 +1,8 @@
 library(usethis)
+library(gh)
 use_git_config(user.name = "TomSOWI", user.email = "tomklotz@gmx.net")
+#gh_token(api_url = )
+#gitcreds::gitcreds_get()
+
+use_github(protocol = "https", auth_token = Sys.getenv("GITHUB_PAt"))
+#?use_github
