54bc8e5199a44190a520719d39548c888085c80c
TomSOWI <tomklotz@gmx.net>
2023-05-05 01:07:45 +0200

    Finishing pre processing

---
+++ 0. Libraries.R
@@ -0,0 +1,14 @@
+#Libraries
+library(RPostgres)
+library(RPostgreSQL)
+library(quanteda)
+library(udpipe)
+library(proxy)
+library(ggplot2)
+library(tidyverse)
+library(proxy)
+library(factoextra)
+library(FactoMineR)
+library(here)
+
+
