a041bfea2556736abf3e5a755e31d18dbb29c368
TomSOWI <tomklotz@gmx.net>
2023-06-25 15:16:44 +0200

    pre processing update

--- Funktionen/expertyears_cleaning.R
+++ Funktionen/expertyears_cleaning.R
@@ -14,4 +14,13 @@ expertyears_cleaning <- function(corp, pattern){
   return(toks)
 }
 
+corpus_to_compund_tokens <- function(corp, pattern){
+  toks <- get_pop_tokens_updated(corp,create_compounds = T,compounds_dict = pattern,compounds_at_level = "sentences")
+  docnames(toks) <- docvars(corp)$id
+  docvars(toks)$party <- docvars(corp)$party
+  docvars(toks)$n_sentences <- quanteda::nsentence(corp)
+  docvars(toks)$n_tokens <- quanteda::ntoken(toks)
+  return(toks)
+}
+
 
